b36ffee02e9725b5de831baa83aed12d63698512
leandro.rferreira <leandro.rferreira@sp.senac.br>
2023-12-02 12:14:32 -0300

    add @Transactional to property methods

--- src/main/java/com/imob/imobapi/service/impl/PropertyServiceImpl.java
+++ src/main/java/com/imob/imobapi/service/impl/PropertyServiceImpl.java
@@ -3,6 +3,7 @@ package com.imob.imobapi.service.impl;
 import com.imob.imobapi.model.Property;
 import com.imob.imobapi.repository.PropertyRepository;
 import com.imob.imobapi.service.PropertyService;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,7 @@ public class PropertyServiceImpl implements PropertyService {
     private PropertyRepository repository;
 
     @Override
+    @Transactional
     public Property save(Property property) {
         return repository.save(property);
     }
@@ -31,11 +33,13 @@ public class PropertyServiceImpl implements PropertyService {
     }
 
     @Override
+    @Transactional
     public Property update(Property property) {
         return repository.save(property);
     }
 
     @Override
+    @Transactional
     public void deleteById(UUID id) {
         repository.deleteById(id);
     }
