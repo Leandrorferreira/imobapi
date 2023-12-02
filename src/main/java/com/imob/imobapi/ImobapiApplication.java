10e18d851209234f0cbc9abfa1c00ce33f17995c
leandro.rferreira <leandro.rferreira@sp.senac.br>
2023-12-02 08:40:11 -0300

    initial commit

---
+++ src/main/java/com/imob/imobapi/ImobapiApplication.java
@@ -0,0 +1,13 @@
+package com.imob.imobapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImobapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImobapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imob/imobapi/ImobapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imob.imobapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImobapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
